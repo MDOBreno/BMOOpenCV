fd7bc5cef5c7d43f521312741eaae56de0cc9235
Breno Medeiros de Oliveira <breno.medeiros@icloud.com>
2020-03-26 23:58:45 -0300

    Primeira Versão  do Projeto integrada ao OpenCV 3.4.9

--- BMOOpenCV/main.cpp
+++ BMOOpenCV/main.cpp
@@ -3,13 +3,15 @@
 //  BMOOpenCV
 //
 //  Created by Breno Medeiros on 26/03/20.
-//  Copyright © 2020 ProgramasBMO. All rights reserved.
+//  Copyright © 2020 Udemy. All rights reserved.
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";                             //----------
+    std::cout << "OpenCV Version " << CV_VERSION << std::endl;  //++++++++++
     return 0;
 }
